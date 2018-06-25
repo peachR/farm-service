98f36a142fa59b7d36b8684d8c95a1f32d4f6ccf
liminlei <minleili@126.com>
2018-06-25 10:19:21 +0800

    傻逼一乙吃枣药丸

--- farm/src/main/java/com/yiyi/farm/TimeInABottle.java
+++ farm/src/main/java/com/yiyi/farm/TimeInABottle.java
@@ -4,6 +4,9 @@ import com.yiyi.farm.facade.redis.RedisService;
 import com.yiyi.farm.service.invite.InviteServiceImpl;
 import com.yiyi.farm.service.invite.TempInviteServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataAccessException;
+import org.springframework.data.redis.connection.RedisConnection;
+import org.springframework.data.redis.core.RedisCallback;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
@@ -27,13 +30,25 @@ public class TimeInABottle {
     @Autowired
     private RedisService redisService;
 
+    @Autowired
+    private RedisTemplate redisTemplate;
+
     @Scheduled(cron = " 0 0 23 * * ?")
     public void rebuildTreeAndCache() throws ExecutionException, InterruptedException {
+        flushDB();
         redisService.set("busy","yes");
         inviteService.newInit();
         //tempInviteServiceinviteService.initCache();
         redisService.remove("busy");
     }
 
-
+    private void flushDB(){
+        redisTemplate.execute(new RedisCallback() {
+            @Override
+            public Object doInRedis(RedisConnection redisConnection) throws DataAccessException {
+                redisConnection.flushDb();
+                return "";
+            }
+        });
+    }
 }
